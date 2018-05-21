f8dde87045bb68557c62dc8fbbac66888d139446
hyunjin <hyunjin.jeong@toki.waseda.jp>
2018-05-21 11:58:24 +0900

    updated review

--- speedRead/server.js
+++ speedRead/server.js
@@ -1,47 +1,36 @@
 let fs = require('fs');
-let express = require('express');
 let app = express();
-let sprecord;
 
-//heroku -> process.env.PORT
+//for heroku -> process.env.PORT
 let server = app.listen(process.env.PORT || 3000, () => {
 	let host = server.address().address || "localhost";
 	let port = server.address().port;
 	console.log('spreeder app listening at http://' + host + ':' + port);
 });
 
+let express = require('express');
 app.use(express.static('public'));
 
 //socket
 let io = require('socket.io')(server);
-io.sockets.on('connection', (socket) =>{
-	console.log("new client: " + socket.id);
-	
-	sprecord = require('./public/files/sprecord.json');
+io.sockets.on('connection', (socket) =>{	
+	let sprecord = require('./public/files/sprecord.json');
 	io.to(socket.id).emit('record', sprecord);
 
 	socket.on('record', (data)=> {
 		// Data comes in as whatever was sent, including objects
-		console.log("Received from:",socket.id," 'record' ");
 		sprecord = merge(sprecord, data);
 	
 		// Send it to all other clients
 		socket.broadcast.emit('record', sprecord);
-		
-		// This is a way to send to everyone including sender
-		// io.sockets.emit('message', "this goes to everyone");
 	});
 
 	socket.on('disconnect', ()=>{
-		console.log('bye',socket.id);
-
 		const record = JSON.stringify(sprecord);
 		fs.writeFile("./public/files/sprecord.json", record, 'utf8', (err) =>{
     	if (err) {
         return console.log(err);
     	}
-
-	    console.log("The file was saved!");
 		}); 
 	});
 });
